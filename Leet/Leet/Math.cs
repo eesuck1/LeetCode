3690ed871168fcad4e3d0a4fca8dfc50c43b500d
eesuck1 <eesucksc@gmail.com>
2023-02-07 22:51:18 +0200

    Solution without recursion

--- Leet/Leet/Math.cs
+++ Leet/Leet/Math.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace Leet
 {
-    internal class Math
+    internal class Solution
     {
         public static double Pow(double x, int n)
         {
             if (n == 0 || x == 1f) return 1;
 
-            if (n < 0)
-            {
-                return 1 / Pow(x, -n);
-            }
-
             double result = x;
 
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i < Math.Abs(n); i++)
             {
                 result *= x;
+            }
+
+            if (n < 0)
+            {
+                return 1 / result;
             }
 
             return result;
